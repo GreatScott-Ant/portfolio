2a57d646f3759ce78116d28af21f6c8b8c30e34a
anthonyholt <ant.scott227@gmail.com>
2024-01-18 20:44:47 -0800

    new song

--- src/components/PlaySound.js
+++ src/components/PlaySound.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import useSound from 'use-sound';
-import Bounce from '../New God Bounce.mp3';
 import pressPlay from '../play4.svg';
 import './PlaySound.css';
 
+const playlist = [
+  { name: 'New God Bounce', file: require('../New God Bounce.mp3') },
+  { name: 'Water', file: require('../Water.mp3') },
+  // { name: 'Song Name', file: require('../song.mp3') },
+];
+
 const PlaySound = () => {
-  const [playSound, { stop }] = useSound(Bounce);
+  const [currentTrack, setCurrentTrack] = useState(0);
+  const [playSound, { stop }] = useSound(playlist[currentTrack].file);
   const [isHovered, setIsHovered] = useState(false);
+
+  const playNextTrack = () => {
+    stop();
+    setCurrentTrack((prevTrack) => (prevTrack + 1) % playlist.length);
+  };
 
   return (
     <div className="vibes">
@@ -27,9 +38,11 @@ const PlaySound = () => {
           stop();
           setIsHovered(false);
         }}
+        onClick={playNextTrack}
       >
         <img alt="" width={30} height={30} src={pressPlay} />
       </button>
+      <p>{playlist[currentTrack].name}</p>
     </div>
   );
 };
